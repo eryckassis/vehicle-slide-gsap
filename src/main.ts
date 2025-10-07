f3944171a829eef4130975d3de4b61d2ac74019e
Eryck Assis <eng.assis.dev@gmail.com>
2025-10-07 14:05:06 -0300

    Enhance vehicle slider with detailed specifications and performance badges

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,1 @@
 import "./style.css";
-
-class VeiculeSlider {
-  constructor() {
-    this.slides = document.querySelectorAll(".slide");
-    this.previewButton = document.querySelectorAll(".nav-prev");
-    this.nextButton = document.querySelectorAll(".nav-next");
-    this.currentIndex = -1;
-    this.inicio();
-  }
-
-  inicio() {
-    this.setupSlideClickListeners();
-    this.setupNavigationListeners();
-    this.setupKeyboardListeners();
-  }
-
-  sed;
-}
