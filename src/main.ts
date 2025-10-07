6d2ab2ab75ce2cb19759cf5edf121381b9374064
Eryck Assis <eng.assis.dev@gmail.com>
2025-10-07 11:57:23 -0300

    Refactor slider implementation and enhance accessibility features

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class VeiculeSlider {
+  constructor() {
+    this.slides = document.querySelectorAll(".slide");
+    this.previewButton = document.querySelectorAll(".nav-prev");
+    this.nextButton = document.querySelectorAll(".nav-next");
+    this.currentIndex = -1;
+    this.inicio();
+  }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  inicio() {
+    this.setupSlideClickListeners();
+    this.setupNavigationListeners();
+    this.setupKeyboardListeners();
+  }
+
+  sed;
+}
